0d4a1af4cb1dafdb1630ac7d9cf43bce8ece7b6f
Deny-ux <denys.fokashchuk@gmail.com>
2023-10-12 15:15:14 +0200

    after rebase

--- test.py
+++ test.py
@@ -2,6 +2,8 @@ def some_local_func():
  pass
 print("first commit")
 
+
+
 def mutual_func():
  ###
  #s#
@@ -11,4 +13,18 @@ def mutual_func():
 def only_local_func():
  return pass
 
+print("something added there")
+def only_master():
+ a = 12
+ a = 12
+ a = 12
+ a = 12
+ a = 12
+ a = 12
+ a = 12
+ a = 12
+ a = 12
+ pass
+
+print("master changed")
 print("NOT LOCAL")
